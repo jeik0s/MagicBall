2eaad8b611a53e556e96d4b6c7656b8fc05a32fa
Jakub Zaremba <jzaremba@MAgiC.local>
2021-06-10 23:33:12 +0200

    Get ball text from array list Previously we get the text from the photo, now we have a single photo with placeholder for a text from array

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ void main() {
   runApp(MagicBall());
 }
 
-
-
 class MagicBall extends StatefulWidget {
   @override
   _MagicBallState createState() => _MagicBallState();
@@ -14,14 +12,27 @@ class MagicBall extends StatefulWidget {
 
 class _MagicBallState extends State<MagicBall> {
   int ballNumber = 1;
+  List<String> ballTexts = [
+    "Yes",
+    "No",
+    "Go for it!",
+    "Absolutly not!",
+    "Shake again",
+    "Dont ask me question like this",
+    "Ask yo mama",
+    "Ask again tomorrow"
+  ];
+  void randomizeBallNumer() {
+    setState(() {
 
-  void randomizeBallNumer(){
+
     int previousNumber = ballNumber;
-    ballNumber = Random().nextInt(5) + 1;
-    if(ballNumber == previousNumber && ballNumber < 5)
+    ballNumber = Random().nextInt(ballTexts.length - 1) + 1;
+    if (ballNumber == previousNumber && ballNumber < ballTexts.length - 1)
       ballNumber++;
-    else if(ballNumber == previousNumber && ballNumber > 1)
-      ballNumber--;
+    else if (ballNumber == previousNumber && ballNumber > 1) ballNumber--;
+    });
+
   }
 
   @override
@@ -34,21 +45,34 @@ class _MagicBallState extends State<MagicBall> {
         ),
         backgroundColor: Colors.blue[200],
         body: Center(
-          child: Container(
-            child: FlatButton(
-              child: Image.asset("imgs/ball$ballNumber.png"),
-              onPressed: () {
-                setState(() {
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    randomizeBallNumer();
+                  },
+                  child: Image.asset("imgs/ballTemplate.png"),
+                  padding: EdgeInsets.only(
+                      left: 20.0, right: 26.0, top: 35.0, bottom: 17.0)),
+              FlatButton(
+                onPressed: () {
                   randomizeBallNumer();
-                });
-              },
-              padding: EdgeInsets.all(30.0) ,
-            ),
-          )
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(90))),
+                  width: 125.0,
+                  height: 125.0,
+                    child: Center(child: Text(ballTexts[ballNumber],
+                    textAlign: TextAlign.center,)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
